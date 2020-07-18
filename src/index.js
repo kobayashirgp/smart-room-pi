9bec5d2eb6bedd7312f9ae20d1d5a14e55728288
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-07-18 01:50:12 -0300

    Adiciona event handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const moment = require("moment");
 const LCD = require("raspberrypi-liquid-crystal-simple");
+const { emit } = require("process");
 
 const lcd = new LCD(1, 0x27, 16, 2);
 
@@ -11,7 +12,10 @@ const port = 3000;
 const server = http.createServer();
 
 server.listen(port, hostname, async () => {
+  lcd.on("error", (ex) => console.log(ex));
+  lcd.on("ready", () => console.log("ready"));
   await lcd.init();
+  lcd.setLine(0, "Hello World");
   setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
   console.log(`Server running at http://${hostname}:${port}/`);
 });
