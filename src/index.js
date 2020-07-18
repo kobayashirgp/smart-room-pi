bd774779cc6cd1ffa8dabb17f5959260b02cf736
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-07-18 02:29:22 -0300

    Atualiza

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ server.listen(port, hostname, async () => {
   lcd.on("ready", () => {
     lcd.setLine(0, "Hello World");
     console.log("ready");
-    //setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
+    setInterval(() => lcd.setLine(1, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
   });
   lcd.initSync();
   console.log(`Server running at http://${hostname}:${port}/`);
