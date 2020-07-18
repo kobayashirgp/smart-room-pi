ee77e8aecf5078f01c459108ebaed30d68da6de2
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-07-18 02:04:59 -0300

    Altera logica

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 const http = require("http");
 const moment = require("moment");
 const LCD = require("raspberrypi-liquid-crystal-simple");
-const { emit } = require("process");
 
 const lcd = new LCD(1, 0x27, 16, 2);
 
-lcd.lines = ["Hello", "World"];
 const hostname = "127.0.0.1";
 const port = 3000;
-
+lcd.cursor = true;
+lcd.display = true;
 const server = http.createServer();
 
 server.listen(port, hostname, async () => {
   lcd.on("error", (ex) => console.log(ex));
   lcd.on("ready", () => {
     lcd.setLine(0, "Hello World");
-    setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
+    console.log("ready");
+    //setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
   });
-  await lcd.init();
-
+  lcd.initSync();
   console.log(`Server running at http://${hostname}:${port}/`);
 });
