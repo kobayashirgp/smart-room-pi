7d8ccbd95f2c566bde00a0002c547fa01ddbd655
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-07-18 01:42:54 -0300

    Adiciona inicializacao

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const port = 3000;
 
 const server = http.createServer();
 
-server.listen(port, hostname, () => {
+server.listen(port, hostname, async () => {
+  await lcd.init();
   setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
   console.log(`Server running at http://${hostname}:${port}/`);
 });
