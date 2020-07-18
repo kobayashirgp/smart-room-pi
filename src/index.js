ce08de8605d6ae0d511eceb8702445537b913e55
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-07-18 01:52:07 -0300

    Refatora bloco

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ const server = http.createServer();
 
 server.listen(port, hostname, async () => {
   lcd.on("error", (ex) => console.log(ex));
-  lcd.on("ready", () => console.log("ready"));
+  lcd.on("ready", () => {
+    lcd.setLine(0, "Hello World");
+    setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
+  });
   await lcd.init();
-  lcd.setLine(0, "Hello World");
-  setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
+
   console.log(`Server running at http://${hostname}:${port}/`);
 });
