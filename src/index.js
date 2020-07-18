0cd54e230f3854de3653da20b2b6d51f6ed8fd05
mateus_mtks11@hotmail.com <mateus_mtks11@hotmail.com>
2020-07-18 01:29:09 -0300

    Inicia projeto

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const moment = require("moment");
+const LCD = require("raspberrypi-liquid-crystal-simple");
+
+const lcd = new LCD(1, 0x27, 16, 2);
+
+lcd.lines = ["Hello", "World"];
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer();
+
+server.listen(port, hostname, () => {
+  setInterval(() => lcd.setLine(0, moment().format("DD/MM/YYYY HH:mm:ss")), 1000);
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
